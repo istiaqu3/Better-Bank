609b52903bc1b9845aa26101355447e4f3f9770b
istiaqu3 <istiaque.255@gmail.com>
2022-08-30 21:34:21 +0600

    solved: only withdraw is possible when Balance Amount > Withdraw Amount

--- js/bankingScript.js
+++ js/bankingScript.js
@@ -1,79 +1,3 @@
-// // Updating Deposit and Balance amount from input field
-// document.getElementById('depositButton').addEventListener('click', function () {
-
-
-//     // gettng deposit amoiunt from deposit field 
-//     const depositAmountText = document.getElementById('depositField').value;
-//     depositAmount = parseFloat(depositAmountText);
-//     // console.log('from field', depositAmount);
-
-//     // setting deposit amount 
-
-//     const currentDepositText = document.getElementById('totalDepositAmount').innerText;
-//     const currentDepositAmount = parseFloat(currentDepositText);
-//     // console.log('current', currentDeposit);
-//     let totalDepositAmount = currentDepositAmount + depositAmount;
-//     document.getElementById('totalDepositAmount').innerText = totalDepositAmount;
-
-//     // clearing deposit input field 
-//     document.getElementById('depositField').value = '';
-
-
-
-//     // <-----------------updating total balance----------------->
-
-//     const currentBalanceText = document.getElementById('totalBalanceAmount').innerText;
-//     const currentBalanceAmount = parseFloat(currentBalanceText);
-//     let totalBalanceAmount = currentBalanceAmount + depositAmount;
-//     document.getElementById('totalBalanceAmount').innerText = totalBalanceAmount;
-
-// })
-
-
-
-
-// // <================Updating Withdraw and Balance amount from input field=================>
-
-
-
-// document.getElementById('withdrawButton').addEventListener('click', function () {
-
-
-//     // gettng withdraw amoiunt from withdraw field 
-//     const withdrawAmountText = document.getElementById('withdrawField').value;
-//     withdrawAmount = parseFloat(withdrawAmountText);
-//     // console.log('from field', withdrawAmount);
-
-//     // setting deposit amount 
-
-//     const currentWithdrawText = document.getElementById('totalWithdrawAmount').innerText;
-//     const currentWithdrawAmount = parseFloat(currentWithdrawText);
-//     // console.log('current', currentWithdrawAmount);
-//     let totalWithdrawAmount = currentWithdrawAmount + withdrawAmount;
-//     document.getElementById('totalWithdrawAmount').innerText = totalWithdrawAmount;
-
-//     // clearing deposit input field 
-//     document.getElementById('withdrawField').value = '';
-
-
-
-//     // // <-----------------updating total balance----------------->
-
-//     const currentBalanceText = document.getElementById('totalBalanceAmount').innerText;
-//     const currentBalanceAmount = parseFloat(currentBalanceText);
-//     let totalBalanceAmount = currentBalanceAmount - withdrawAmount;
-//     document.getElementById('totalBalanceAmount').innerText = totalBalanceAmount;
-
-// })
-
-
-
-
-
-
-// using functions 
-
-
 // getting amount from input field
 function getAmount(inputId) {
     const amountText = document.getElementById(inputId).value;
@@ -146,7 +70,9 @@ document.getElementById('withdrawButton').addEventListener('click', function () 
 
     // gettng withdraw amoiunt from withdraw field 
     const withdrawAmount = getAmount('withdrawField');
-    if (withdrawAmount > 0) {
+    const currentBalanceText = document.getElementById('totalBalanceAmount').innerText;
+    const currentBalanceAmount = parseFloat(currentBalanceText);
+    if (withdrawAmount > 0 && withdrawAmount < currentBalanceAmount) {
         // setting deposit amount 
 
         setTotalAmount('totalWithdrawAmount', withdrawAmount);
@@ -163,7 +89,4 @@ document.getElementById('withdrawButton').addEventListener('click', function () 
         alert('Wrong Input!')
 
     }
-
-
-
 })
